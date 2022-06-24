391b1cbbb5fde97f35cd7cef31851081785838e1
Daniel Harrington <dmharrington03@gmail.com>
2022-06-24 18:28:34 -0400

    started index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,62 +4,40 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Header from "../components/header"
+
+import UIkit from "uikit";
+import Icons from 'uikit/dist/js/uikit-icons';
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <Seo title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
+
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="All posts" />
-      <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
-    </Layout>
+    <div>
+      <Header />
+      <div className="uk-container uk-width-1-2">
+          <section>
+            <div className="maintext uk-margin">
+              <h2 className="hash">#</h2>
+              <h2>ABOUT ME</h2>
+              <p>Hello! I am an undergraduate physics student at Tufts University. I am interested in learning about condensed matter and high energy physics, specifically the design and engineering of experimental equipment. </p>
+            </div>
+          </section>
+      </div>
+      <br />
+      <div className="uk-container uk-width-1-2">
+        <section>
+          <div className="maintext uk-margin">
+            <h2 className="hash">#</h2>
+            <h2>MY PROJECTS</h2>
+            <p>Hello! I am an undergraduate physics student at Tufts University. I am interested in learning about condensed matter and high energy physics, specifically the design and engineering of experimental equipment. </p>
+          </div>
+        </section>
+      </div>
+    </div>
   )
 }
 
@@ -87,3 +65,23 @@ export const pageQuery = graphql`
     }
   }
 `
+
+
+// <div className="uk-margin-large-bottom section">
+//             <h2>MY PROJECTS</h2>
+//             <section>
+//               <h3>SOFTWARE</h3>
+//               {/* For loop of projects */}
+//               <div className="uk-flex">
+//                 {posts.map((post) => {
+//                   const title = post.frontmatter.title || post.fields.slug;
+//                   // const imgMatch = images.find((node) => post.frontmatter.title === node.name);
+//                   return (
+//                     <div className="uk-card uk-card-default uk-card-body ">
+//                       {title}
+//                     </div>
+//                   );
+//                 })}
+//               </div>
+//             </section>
+//           </div>
