9366c113eaebb7f1e0ae7109dab55bac407b44cd
Daniel Harrington <dmharrington03@gmail.com>
2022-06-24 18:28:15 -0400

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,27 @@
+import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+import UIkit from "uikit";
+import Icons from 'uikit/dist/js/uikit-icons';
+
+
+const Header = () => {
+
+    UIkit.use(Icons);
+
+    return (
+        <header className="header">
+            <div className="uk-flex uk-flex-middle uk-flex-column">
+                <div className="uk-width-1-1 top" ></div>
+                <h1 className="logo">DANIEL HARRINGTON</h1>
+                    <StaticImage
+                        className="pic1 uk-display-block"
+                        src="../images/headshot.png"
+                        alt="Profile picture"
+                    />
+            </div>
+        </header>
+    );
+}
+
+export default Header
