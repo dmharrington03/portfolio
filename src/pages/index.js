e7fe13f25c4bf2c11c814e36338f31a1f4b3b25f
Daniel Harrington <dmharrington03@gmail.com>
2022-06-25 22:20:40 -0400

    started medium captions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
 import Header from "../components/header"
+import MediumCaption from "../components/mediumcaption"
 
 import UIkit from "uikit";
 import Icons from 'uikit/dist/js/uikit-icons';
@@ -12,28 +11,45 @@ import Icons from 'uikit/dist/js/uikit-icons';
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
+  const images = data.allFile.nodes
 
-
+  const bio = "Hello! I am an undergraduate physics student at Tufts University. I am interested in learning about condensed matter and high energy physics, specifically the design and engineering of experimental equipment."
 
   return (
     <div>
       <Header />
-      <div className="uk-container uk-width-1-2">
+      <div className="uk-container uk-width-3-4 uk-width-1-2@l">
           <section>
             <div className="maintext uk-margin">
               <h2 className="hash">#</h2>
               <h2>ABOUT ME</h2>
-              <p>Hello! I am an undergraduate physics student at Tufts University. I am interested in learning about condensed matter and high energy physics, specifically the design and engineering of experimental equipment. </p>
+              <p>{ bio }</p>
             </div>
           </section>
       </div>
       <br />
-      <div className="uk-container uk-width-1-2">
+
+      <div className="uk-container uk-width-3-4 uk-width-1-2@l">
         <section>
           <div className="maintext uk-margin">
             <h2 className="hash">#</h2>
             <h2>MY PROJECTS</h2>
-            <p>Hello! I am an undergraduate physics student at Tufts University. I am interested in learning about condensed matter and high energy physics, specifically the design and engineering of experimental equipment. </p>
+            <h3 className="uk-margin">SOFTWARE</h3>
+
+            <div className="uk-flex uk-flex-column">
+              {posts.map((post) => {
+                const title = post.frontmatter.title || post.fields.slug;
+                const imgMatch = images.find((node) => post.frontmatter.title === node.name);
+                return (
+                  <MediumCaption 
+                  source={ imgMatch ? imgMatch.publicURL : "" }
+                  title={ title }
+                  body={ "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"}
+                  />)
+                })
+              }
+            </div>
+
           </div>
         </section>
       </div>
@@ -50,38 +66,40 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(filter: {frontmatter: {title: {glob: "display*"}}}) {
       nodes {
-        excerpt
+        id
+        frontmatter {
+          title
+        }
+        html
         fields {
           slug
         }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-        }
+      }
+    }
+    allFile(filter: {relativeDirectory: {glob: "display*"}, extension: {in: ["jpg", "png"]}}) {
+      nodes {
+        publicURL
+        name
       }
     }
   }
 `
 
 
-// <div className="uk-margin-large-bottom section">
-//             <h2>MY PROJECTS</h2>
-//             <section>
-//               <h3>SOFTWARE</h3>
-//               {/* For loop of projects */}
-//               <div className="uk-flex">
-//                 {posts.map((post) => {
-//                   const title = post.frontmatter.title || post.fields.slug;
-//                   // const imgMatch = images.find((node) => post.frontmatter.title === node.name);
-//                   return (
-//                     <div className="uk-card uk-card-default uk-card-body ">
-//                       {title}
-//                     </div>
-//                   );
-//                 })}
-//               </div>
-//             </section>
-//           </div>
+
+
+// allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+//   nodes {
+//     excerpt
+//     fields {
+//       slug
+//     }
+//     frontmatter {
+//       date(formatString: "MMMM DD, YYYY")
+//       title
+//       description
+//     }
+//   }
+// }
