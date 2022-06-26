d00e1137f979354600f39cd1267ddb5b3fe97d72
Daniel Harrington <dmharrington03@gmail.com>
2022-06-26 17:11:36 -0400

    added inversion

--- src/components/mediumcaption.js
+++ src/components/mediumcaption.js
@@ -4,22 +4,23 @@ import { StaticImage } from "gatsby-plugin-image"
 import UIkit from "uikit";
 
 
-const MediumCaption = ({source, title, caption}) => {
+const MediumCaption = ({source, title, caption, invert}) => {
 
+    let invertedClass = invert ? "uk-flex-first@m" : ""
 
     return (
         <div className="uk-height-expand">
-              <p className="caption">
 
-                <div className="uk-flex-middle uk-grid" data-uk-grid>
-                  <div className="uk-width-1-3@m uk-flex-first"><img src={ source } className="medium-caption"/></div>
+                <div className="uk-flex-middle uk-grid uk-margin-large" data-uk-grid>
+                  <div className="uk-width-1-2@m">
+                    <img src={ source } className="medium-caption" data-uk-img="loading: eager"/>
+                  </div>
 
-                  <div className="uk-width-2-3@m">
-                    <h4 className="uk-margin-small-bottom uk-margin-top"><i>{title}</i></h4>
+                  <div className={"uk-width-expand " + invertedClass}>
+                    <h4 className="uk-margin-small-bottom"><i>{title}</i></h4>
                     <section dangerouslySetInnerHTML={{ __html: caption }}></section>
                   </div>
                 </div>
-              </p>
         </div>
     );
 }
