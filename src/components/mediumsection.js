8701df704793ce7b2ed3cab946b39928002ba54f
Daniel Harrington <dmharrington03@gmail.com>
2022-06-26 17:11:44 -0400

    initial commit

---
+++ src/components/mediumsection.js
@@ -0,0 +1,53 @@
+import React from "react"
+
+import MediumCaption from "./mediumcaption"
+import { StaticQuery, graphql } from "gatsby"
+
+const MediumSection = () => {
+
+  return (
+  <StaticQuery
+      query={graphql`
+        query sectionQuery {
+          allMarkdownRemark(filter: {frontmatter: {title: {glob: "M-**"}}}) {
+            nodes {
+              id
+              frontmatter {
+                title
+                description
+              }
+              html
+              fields {
+                slug
+              }
+            }
+          }
+          allFile(filter: {relativeDirectory: {glob: "project*"}, extension: {in: ["jpg", "png"]}}) {
+            nodes {
+              publicURL
+              name
+            }
+          }
+        }
+      `}
+      render={(data) => (
+        <div className="uk-flex uk-flex-column">
+          {data.allMarkdownRemark.nodes.map((post, index) => {
+            // const title = post.frontmatter.title || post.fields.slug;
+            const imgMatch = data.allFile.nodes.find((node) => post.frontmatter.title === node.name);
+            return (
+              <MediumCaption
+                source={imgMatch ? imgMatch.publicURL : ""}
+                title={post.frontmatter.description || 'error'}
+                caption={post.html}
+                invert={index % 2} />
+                )
+            })
+          }
+        </div>
+      ) 
+    }
+  />)
+}
+
+export default MediumSection
