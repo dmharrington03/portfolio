680851e624881c9b738d85eb57dac77cfbe258d7
Daniel Harrington <dmharrington03@gmail.com>
2022-06-30 20:53:46 -0400

    formatting

--- src/components/squarecaption.js
+++ src/components/squarecaption.js
@@ -4,8 +4,8 @@ const SquareCaption = ({imgURL, title, description}) => {
 
   return (
     <div>
-      <div className="uk-flex uk-flex-column uk-flex-between">
-        <img src={imgURL} alt={title} className="sq-img uk-width-1-1"/>
+      <div className="uk-flex uk-flex-column uk-flex-between uk-width-1-1">
+        <img src={imgURL} alt={title} className="sq-img uk-width-5-6 uk-width-1-1@s"/>
         <h4 className="uk-margin-small"><b><i>{title}</i></b></h4>
         <section className="sq-caption uk-margin-bottom" 
           dangerouslySetInnerHTML={{ __html: description }}></section>
