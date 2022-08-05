649de82375ce911a75a22f46af19105e63cc2b94
Daniel Harrington <dmharrington03@gmail.com>
2022-08-05 17:05:23 -0400

    added subheadings / styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,8 @@ const BlogIndex = ({ data, location }) => {
 
   const bio = "Hello! I am an undergraduate physics student at Tufts University. I am interested in learning about condensed matter and high energy physics, specifically the design and engineering of experimental equipment."
 
+  const research = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+
   UIkit.use(Icons)
 
   return (
@@ -29,28 +31,28 @@ const BlogIndex = ({ data, location }) => {
             <h2 className="hash">#</h2>
             <h2>ABOUT ME</h2>
             <p>{ bio }</p>
+
+            {/* Text Section */}
+            <h3 className="uk-margin-medium-top uk-margin-small-bottom">RESEARCH</h3>
+            <h4 className="uk-margin-remove"><i>RA, Peter Love Quantum Information Group, Tufts University</i></h4>
+            <p className="uk-margin-remove-top uk-margin-large-bottom">{ research }</p>
           </div>
         </section>
-        <br />
+
         <section>
           <div className="maintext uk-margin">
             <h2 className="hash">#</h2>
             <h2>MY PROJECTS</h2>
+            <h3 className="h-primary">SOFTWARE</h3>
             
+            {/* Small Section */}
+            <h3 className="uk-margin">C++/SFML SIMULATIONS</h3>
+            <SmallSection />
+            <br />
             {/* Medium Caption Section */}
             <h3 className="uk-margin">DATA SCIENCE</h3>
             <MediumSection />
 
-            {/* Text Section */}
-            <h3 className="uk-margin">RESEARCH</h3>
-            <h4 className="uk-margin-remove-bottom"><i>RA, Peter Love Quantum Information Group, Tufts University</i></h4>
-            <p className="uk-margin-remove-top uk-margin-large-bottom">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-
-
-            {/* Small Section */}
-            <h3 className="uk-margin">SIMULATIONS</h3>
-            <SmallSection />
-            <br />
             {/* Square Section */}
             <h3 className="uk-margin">WEB DEVELOPMENT</h3>
             <SquareSection/>
