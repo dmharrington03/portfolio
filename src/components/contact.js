ad2ee777cb57c95412f3b4e68fa88bdbf9ac54ed
Daniel Harrington <dmharrington03@gmail.com>
2022-08-05 20:41:39 -0400

    secondary email

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,7 +10,13 @@ const Contact = () => {
         <span className="uk-icon soc" uk-icon="icon: github; ratio: 1.5"></span><code>@dmharrington03</code>
       </a></li>
       <li><a href="https://www.linkedin.com/in/dmharrington03/" target="_blank" rel="noopener noreferrer"><span className="uk-icon soc" uk-icon="icon: linkedin; ratio: 1.5"></span><code>@dmharrington03</code></a></li>
-      <li><a href="mailto: dmharrington03@gmail.com" target="_blank" rel="noopener noreferrer"><span className="uk-icon soc" uk-icon="icon: mail; ratio: 1.5"></span><code>dmharrington03@gmail.com</code></a></li>
+      <li>
+        <span className="uk-icon soc uk-float-left" uk-icon="icon: mail; ratio: 1.5"></span>
+        <ul className="list-remove">
+          <li><a href="mailto: dmharrington03@gmail.com" target="_blank" rel="noopener noreferrer" className="a-remove"><code>dmharrington03@gmail.com</code></a></li>
+          <li><a href="mailto: dharri24@tufts.edu" target="_blank" rel="noopener noreferrer" className="a-remove"><code>dharri24@tufts.edu</code></a></li>
+        </ul>
+      </li>
     </ul>
   );
 };
