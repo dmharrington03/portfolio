20a4cd23d770c7748aaf3657cb1e14e8478dd29d
Daniel Harrington <dmharrington03@gmail.com>
2022-08-05 20:03:04 -0400

    added triangle icons for future links

--- src/components/mediumcaption.js
+++ src/components/mediumcaption.js
@@ -17,7 +17,10 @@ const MediumCaption = ({source, title, caption, invert}) => {
                   </div>
 
                   <div className={"uk-width-expand " + invertedClass}>
-                    <h4 className="uk-margin-small-bottom"><b><i>{title}</i></b></h4>
+                    <h4 className="uk-margin-small-bottom">
+                      <b><i>{title}</i></b>
+                      <span className="uk-icon" uk-icon="icon: triangle-right"></span>
+                    </h4>
                     <section dangerouslySetInnerHTML={{ __html: caption }}></section>
                   </div>
                 </div>
