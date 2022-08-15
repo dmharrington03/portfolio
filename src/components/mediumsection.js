b831763dceeb27a7c3933d2df417631bb1e4ad77
Daniel Harrington <dmharrington03@gmail.com>
2022-08-14 21:18:31 -0400

    removed old commented query

--- src/components/mediumsection.js
+++ src/components/mediumsection.js
@@ -67,51 +67,7 @@ const MediumSection = () => {
 
     </div>
   );
-
-
-  // return (
-  // <StaticQuery
-  //     query={graphql`
-  //       query sectionQuery {
-  //         allMarkdownRemark(filter: {frontmatter: {title: {glob: "M-**"}}}) {
-  //           nodes {
-  //             id
-  //             frontmatter {
-  //               title
-  //               description
-  //             }
-  //             html
-  //             fields {
-  //               slug
-  //             }
-  //           }
-  //         }
-  //         allFile(filter: {relativeDirectory: {glob: "project*"}, extension: {in: ["jpg", "png"]}}) {
-  //           nodes {
-  //             publicURL
-  //             name
-  //           }
-  //         }
-  //       }
-  //     `}
-  //     render={(data) => (
-  //       <div className="uk-flex uk-flex-column" uk-scrollspy="target: > div; cls: uk-animation-fade; delay: 500">
-  //         {data.allMarkdownRemark.nodes.map((post, index) => {
-  //           // const title = post.frontmatter.title || post.fields.slug;
-  //           const imgMatch = data.allFile.nodes.find((node) => post.frontmatter.title === node.name);
-  //           return (
-  //             <MediumCaption
-  //               source={imgMatch ? imgMatch.publicURL : ""}
-  //               title={post.frontmatter.description || 'error'}
-  //               caption={post.html}
-  //               invert={index % 2} />
-  //               )
-  //           })
-  //         }
-  //       </div>
-  //     ) 
-  //   }
-  // />)
+  
 }
 
 export default MediumSection
