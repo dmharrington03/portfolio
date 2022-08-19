ad84f0cb3478a3b64306a8e224cf8052db37c201
Daniel Harrington <dmharrington03@gmail.com>
2022-08-19 00:12:32 -0400

    reverted previous changes

--- src/components/articleheader.js
+++ src/components/articleheader.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
-import ViewOnGithub from './viewongithub';
 
 
 const ArticleHeader = ({imgURL, post}) => {
@@ -27,7 +26,15 @@ const ArticleHeader = ({imgURL, post}) => {
 
           <h4 className="uk-padding-remove"><i>{post.frontmatter.date}</i></h4>
 
-          <ViewOnGithub url={`https://github.com/dmharrington03/physics/tree/master/${post.frontmatter.description}`} />
+          <a href={`https://github.com/dmharrington03/physics/tree/master/${post.frontmatter.description}`}
+            className="uk-button uk-button-default uk-flex uk-flex-middle git-button uk-visible@s"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span data-uk-icon="github"></span>
+            &nbsp;View on Github
+          </a>
+          <a href="#" className="uk-icon-link uk-hidden@s" data-uk-icon="icon: github; ratio: 2"></a>
         </div>
       </header>
   </div>;
