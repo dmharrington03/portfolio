27bb6c53a300a2775e720709a28924d2524a770a
Daniel Harrington <dmharrington03@gmail.com>
2022-08-18 23:31:43 -0400

    refactored gh button as separate component

--- src/components/articleheader.js
+++ src/components/articleheader.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import ViewOnGithub from './viewongithub';
 
 
 const ArticleHeader = ({imgURL, post}) => {
@@ -26,15 +27,7 @@ const ArticleHeader = ({imgURL, post}) => {
 
           <h4 className="uk-padding-remove"><i>{post.frontmatter.date}</i></h4>
 
-          <a href={`https://github.com/dmharrington03/physics/tree/master/${post.frontmatter.description}`}
-            className="uk-button uk-button-default uk-flex uk-flex-middle git-button uk-visible@s"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span data-uk-icon="github"></span>
-            &nbsp;View on Github
-          </a>
-          <a href="#" className="uk-icon-link uk-hidden@s" data-uk-icon="icon: github; ratio: 2"></a>
+          <ViewOnGithub url={`https://github.com/dmharrington03/physics/tree/master/${post.frontmatter.description}`} />
         </div>
       </header>
   </div>;
