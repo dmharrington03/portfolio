f0f9a77b98acead73b823f9312176ff3d08ba72b
Daniel Harrington <dmharrington03@gmail.com>
2022-08-18 23:17:40 -0400

    added 'this site'

--- src/components/squaresection.js
+++ src/components/squaresection.js
@@ -27,7 +27,7 @@ const SquareSection = () => {
         }
       `}
       render={(data) => (
-        <div className="uk-grid uk-child-width-1-3@s" data-uk-grd uk-scrollspy="target: > div; cls: uk-animation-fade; delay: 300">
+        <div className="uk-grid uk-child-width-1-3@s" uk-scrollspy="target: > div; cls: uk-animation-fade; delay: 300">
           {data.allMarkdownRemark.nodes.map((post) => {
             const imgMatch = data.allFile.nodes.find((node) => post.frontmatter.description === node.name);
             return (
@@ -38,6 +38,7 @@ const SquareSection = () => {
                 )
             })
           }
+          <p className="uk-text-small" uk-scrollspy="cls: uk-animation-slide-left-small; delay: 1000"><i>and this site</i></p>
         </div>
       ) 
     }
