0b3f486e45a900a37f25c0bcf122a3bf0d7d3050
Daniel Harrington <dmharrington03@gmail.com>
2022-08-19 00:13:37 -0400

    changed to icon button - text button will stay in article header component

--- src/components/viewongithub.js
+++ src/components/viewongithub.js
@@ -4,18 +4,10 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 
 const ViewOnGithub = ({url}) => {
+  
   return (
-    <div>
-      <a href={url}
-            className="uk-button uk-button-default uk-flex uk-flex-middle git-button uk-visible@s"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span data-uk-icon="github"></span>
-            &nbsp;View on Github
-          </a>
-          <a href="#" className="uk-icon-link uk-hidden@s" data-uk-icon="icon: github; ratio: 2"></a>
-    </div>
+      <a href={url} target="_blank" rel="noreferrer" 
+      className="uk-icon-button" data-uk-icon="icon: github; ratio: 2"></a>
   );
 };
 
