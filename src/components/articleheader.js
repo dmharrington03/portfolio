f8c4be92ccd276c2bca826990b5e23d7dd509cf8
Daniel Harrington <dmharrington03@gmail.com>
2022-08-21 00:20:39 -0400

    fixed broken link

--- src/components/articleheader.js
+++ src/components/articleheader.js
@@ -34,7 +34,11 @@ const ArticleHeader = ({imgURL, post}) => {
             <span data-uk-icon="github"></span>
             &nbsp;View on Github
           </a>
-          <a href="#" className="uk-icon-link uk-hidden@s" data-uk-icon="icon: github; ratio: 2"></a>
+          <a href={`https://github.com/dmharrington03/physics/tree/master/${post.frontmatter.description}`}
+            target="_blank"
+            rel="noreferrer"
+            className="uk-icon-link uk-hidden@s" 
+            data-uk-icon="icon: github; ratio: 2"></a>
         </div>
       </header>
   </div>;
