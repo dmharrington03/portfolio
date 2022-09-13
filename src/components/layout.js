b17fb900140c6505f5eb5f40b536546a4150ed92
Daniel Harrington <dmharrington03@gmail.com>
2022-09-12 22:51:00 -0400

    initial commit

---
+++ src/components/layout.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../normalize.css"
+import "uikit/dist/css/uikit.min.css"
+import "../style.css"
+import "prism-themes/themes/prism-one-dark.min.css"
+
+
+export default function Layout({ children }) {
+  return <div>{children}</div>
+}
