1ba090134a820c519d8a5fa90255a1d01e16936d
Daniel Harrington <dmharrington03@gmail.com>
2022-10-06 11:11:25 -0400

    switched mnist for exoplanet analysis

--- src/components/mediumsection.js
+++ src/components/mediumsection.js
@@ -1,5 +1,4 @@
 import React from "react"
-import MNIST from "./mnist";
 import UIkit from "uikit";
 import Icons from "uikit/dist/js/uikit-icons";
 
@@ -11,13 +10,33 @@ const MediumSection = () => {
 
   UIkit.use(Icons);
 
-  const MNISTCap = `An interactive web app demonstrating neural network predictions of handwritten numbers based on the MNIST dataset. Originally written as a standalone page in Python using Flask, Tensorflow, and p5.js, then ported to TensorflowJS for web hosting with Gatsby/React.`;
+  const PlanetCap = `An exploration of exoplanet data provided by NASA and the Planetary Habitability Library (PHL) at the University of Puerto Rico. Calculation of the Earth Similarity Index and comparison with various planetary and stellar parameters. Built with Python using matplotlib, numpy, and Pandas.`;
 
   const mapCap = `A series of maps using data from the NOAA GFS built with Python using Cartopy, matplotlib, Numpy, and Siphon/netCDF4. View Google Colab notebook <a href="https://colab.research.google.com/drive/1sCjOxNXqot214c-8KwL5j-ZKkSSK60Lb?usp=sharing" target="_blank" rel="noopener noreferrer">here.</a>`
 
   return (
     <div className="uk-flex uk-flex-column" uk-scrollspy="target: > div; cls: uk-animation-fade; delay: 500">
       
+      {/* Exoplanet Data */}
+      <div className="uk-height-expand">
+
+        <div className="uk-flex-middle uk-grid uk-margin-large" data-uk-grid>
+          <div className="uk-width-1-2@m">
+            <StaticImage src="../displays/project0/esi_phase.jpg" alt="ESI Phase" imgClassName="medium-caption"/>
+          </div>
+
+          <div className="uk-width-expand uk-flex-first@m">
+            <a href="https://colab.research.google.com/drive/1hAK7Wj2q_ZO6vMvVEQdE0LC5vM0YKWeq?usp=sharing" className="uk-link-reset" rel="noreferrer" target="_blank">
+              <h4 className="uk-margin-small-bottom">
+                <b><i>Exoplanet Data Analysis</i></b>
+                <span className="uk-icon" uk-icon="icon: triangle-right"></span>
+              </h4>
+            </a>
+            <section>{ PlanetCap }</section>
+          </div>
+        </div>
+      </div>
+
       {/* Cartography */}
       <div className="uk-height-expand">
 
@@ -37,32 +56,6 @@ const MediumSection = () => {
         </div>
       </div>
 
-      {/* MNIST Interface */}
-      <div className="uk-height-expand">
-
-        <div className="uk-flex-middle uk-grid uk-margin-large" data-uk-grid>
-          <div className="uk-width-1-2@m">
-            <StaticImage src="../displays/project0/M-MNIST_Interface.png" alt="MNIST" imgClassName="medium-caption"/>
-          </div>
-
-          <div className="uk-width-expand uk-flex-first@m">
-            <a href="#mnist" className="uk-link-reset" data-uk-toggle="target: #mnist">
-              <h4 className="uk-margin-small-bottom">
-                <b><i>MNIST Interface</i></b>
-                <span className="uk-icon" uk-icon="icon: triangle-right"></span>
-              </h4>
-            </a>
-            <section>{ MNISTCap }</section>
-          </div>
-
-          <div id="mnist" data-uk-modal>
-              <div class="uk-modal-dialog uk-modal-body uk-width-2-3">
-                  <MNIST />
-                  <button class="uk-modal-close-default" type="button" data-uk-close></button>
-              </div>
-          </div>
-        </div>
-      </div>
 
     </div>
   );
--- src/displays/project0/MNIST_Interface.md
+++
@@ -1,7 +0,0 @@
----
-title: M-MNIST_Interface
-date: "2015-05-01T22:12:03.284Z"
-description: "MNIST Visual Interface"
----
-
-An interactive web app demonstrating neural network predictions of handwritten numbers based on the MNIST dataset. Originally written as a standalone page in Python using Flask, Tensorflow, and p5.js, then ported to TensorflowJS for web hosting with Gatsby/React.
