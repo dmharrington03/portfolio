c1081e7524e2e36d5b6ae67ed0d0ecb53d369999
Daniel Harrington <dmharrington03@gmail.com>
2022-10-16 23:08:21 -0400

    sourced content dir in filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/content/`,
+        name: `content`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
